0e431651c529afb1846a3fceeab6048881e78445
CodyTseng <cody880528@hotmail.com>
2018-11-29 11:17:54 +0800

    Sort course Ann

--- app/src/main/java/com/team214/nctue4/course/CourseAnnFragment.kt
+++ app/src/main/java/com/team214/nctue4/course/CourseAnnFragment.kt
@@ -2,7 +2,6 @@ package com.team214.nctue4.course
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -58,6 +57,7 @@ class CourseAnnFragment : Fragment() {
             .doFinally { progress_bar?.visibility = View.GONE }
             .subscribeBy(
                 onSuccess = {
+                    it.sortByDescending { annItem -> annItem.date }
                     displayData(it)
                 },
                 onError = {
